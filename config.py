26504321a8965906ca0cb43a710f579cdd0b1905
PheonixDeathNote <153576791+PheonixDeathNote@users.noreply.github.com>
2024-04-07 11:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "24478892")
-    API_HASH = environ.get("API_HASH", "b7b00353625f83dbf6ca2438353515a0")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7042426390:AAHm0OOyDf4Tl3ajJF5Eu8CX_0SecUeqpyg") 
-    BOT_SESSION = environ.get("BOT_SESSION", "BQF1hKwAspsV8GyCWHOsUcU-qJ2aqfANPRUNqv_3ut5h2rJLr1S2fhosqhP22kAVPUyDRhK3TPqLAt-i_vibNu_bsuzBY0YbnopOrbfGPlZLdA669yETyMNYC52ZHv6xO50GV8zjV6MeoYzogvJgNzaJaGl65mGGXDWQOJTbrlf4h3-dFH2hM2cItpP1GKTOa6QufM3e93UHZvYwz_RrFJJh38y82_u-EmkNhQ53ZLkaScwlerTAn3mGBOR0BRL8cp0w0pMDvMwvY8LZVGVNwPOMP6KIOf62xVuofByvE4eOJ1tnyXW3ZfHx9qx5nHKDxavbyyayrHArB1k1e98PP314tKE7eAAAAABFJ2WxAA") 
+    API_ID = environ.get("API_ID", "23858329")
+    API_HASH = environ.get("API_HASH", "e2cd67ed2be80b60b52ff80fc3e6c127")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7079907278:AAFcj7FbASiJjU7m9DVI6-H3jMaR86QADo0") 
+    BOT_SESSION = environ.get("BOT_SESSION", "BQFsDJkAPzkzlefaqkTq_Py2BCs9DksLSrIPwuz3uEoEr1PQ8hNo3cUMmRRvNsU5SVMWineZa1GVNFLcXhH4SQQRJKJ1EXV0kNQSGe4JAmUDxyBmGDNP0rtxoVPA_d1noH9JhiqJ0_b_Dd7qlryUqVcKal04YJdyJgMJr5t4e2Oif0BOoBLB9ngdeOee-Ox0yDeZlrzWkNQPtoRD9ZVff23Gt4oqcfABtobGBB9ARxJxb6MFDjhiDH4VMA8mo02Lc3ctW3DuQwMUQkklEpliuLRoMx_8QbVsgCA8DG1yo8RCOzW2g1USAyfsYHAXBM9ElK1y0cgYvyTToanaqRgMvnrBjvRJ1AAAAAFOjqOBAA") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6585878012').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5612938113').split()]
 
 class temp(object): 
     lock = {}
